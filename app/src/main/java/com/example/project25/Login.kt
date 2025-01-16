58173365ed4475f1be18068526d2d5f9d4b1daf1
Alpan <mmuwaffiqulihsan@gmail.com>
2025-01-16 11:10:30 +0700

    Connect to firebase

--- app/src/main/java/com/example/project25/Login.kt
+++ app/src/main/java/com/example/project25/Login.kt
@@ -1,20 +1,34 @@
 package com.example.project25
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class Login : AppCompatActivity() {
+
+    private lateinit var loginButton: ImageView
+    private lateinit var registerButton: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        registerButton = findViewById(R.id.tv_register)
+        loginButton = findViewById(R.id.btnlogin)
+
+        registerButton.setOnClickListener{
+            val intent = Intent(this, Register::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.slide_in_up, R.anim.slide_out_down)
+            finish()
+        }
+        loginButton.setOnClickListener {
+            val intent = Intent(this, PilihGameActivity::class.java)
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/example/project25/MainActivity.kt
+++ app/src/main/java/com/example/project25/MainActivity.kt
@@ -3,20 +3,26 @@ package com.example.project25
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
-import android.os.Looper
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val splashScreenDuration = 5000L
 
-        Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, HalamanUtama::class.java)
-            startActivity(intent)
-            finish()
-        }, splashScreenDuration)
+        val textTitle = findViewById<ImageView>(R.id.textTitle)
+
+        Handler().postDelayed({
+            val fadeOut = AnimationUtils.loadAnimation(this, R.anim.fade_out)
+            textTitle.startAnimation(fadeOut)
+            Handler().postDelayed({
+                val intent = Intent(this, HalamanUtama::class.java)
+                startActivity(intent)
+                overridePendingTransition(R.anim.slide_in_up, R.anim.slide_out_down)
+                finish()
+            }, 1000)
+        }, 1000)
     }
 }
